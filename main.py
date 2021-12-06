e81022341603f9205444b4e03b5e092776d6a622
Roger Rojas <rrojas@indracompany.com>
2021-12-06 15:38:23 -0500

    seteando group_id None

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ from kafka import KafkaConsumer
 
 load_dotenv()
 
+
+broker_1 = "{}:{}".format(os.getenv('REMEDY_KAFKA_HOST_BROKER_1'),  os.getenv('REMEDY_KAFKA_PORT_1'))
+broker_2 = "{}:{}".format(os.getenv('REMEDY_KAFKA_HOST_BROKER_2'),  os.getenv('REMEDY_KAFKA_PORT_2'))
+
+print(f'Conectando al broker {broker_1} ...')
+print(f'Conectando al broker {broker_2} ...')
+
 consumer = KafkaConsumer(
     os.getenv('REMEDY_KAFKA_TOPIC_CREATE'),
-    bootstrap_servers = [os.getenv('REMEDY_KAFKA_HOST_BROKER_1'), os.getenv('REMEDY_KAFKA_HOST_BROKER_2'),],
-    auto_offset_reset = 'earliest',
-    enable_auto_commit = True,
-    #value_deserializer = lambda x: loads(x.decode('utf-8'))
+    group_id=None,
+    bootstrap_servers = [broker_1],
+    auto_offset_reset='earliest',
+    api_version=(0,9)
 )
 
 for message in consumer:
     data = message.value
-    print(f'Data {data}')
+    print(data)
